33488f338c6e2809ed90cf67db666b1e955bf810
siriopticstrade <masterohio60@gmail.com>
2025-12-24 21:55:33 +0545

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 export default function Contact(){
   return (
     <section className="contact">
-      <h2>Contact</h2
+      <h2>Contact</h2>
       <p>Email: masterohio60@gmail.com </p>
       <p>Phone: 9856029748</p>
     </section>
